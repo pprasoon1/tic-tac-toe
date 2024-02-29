448a143c16e9151e2a06398e2e6cb048459aaf17
Prasoon <pprasoon999@gmail.com>
2024-02-29 23:57:24 +0530

    Implement multiplayer functionality

--- app.js
+++ app.js
@@ -1,74 +1,93 @@
+const socket = io();
+
+let currentPlayerNumber;
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turn0 = true;               //playerX, player0
-
 const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ];
-const resetGame = () =>{
-    turn0= true;
+
+const resetGame = () => {
     enableBoxes();
     msgContainer.classList.add("hide");
-}
+    // Emit a reset event to inform the server to reset the game state
+    socket.emit('reset-game');
+};
 
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-    if(turn0){                                        //player0
-        box.innerText = "0";
-        turn0 = false;
-    }  else{                                         //playerX
-        box.innerText = "X";
-        turn0 = true;
-    }
-    box.disabled = true;
+socket.on('player-number', (number) => {
+    currentPlayerNumber = number;
+});
 
+socket.on('update-players', (updatedPlayers) => {
+    console.log(updatedPlayers);  // Log the updated player list
+});
+
+socket.on('update', (data) => {
+    const { boxIndex, value } = data;
+    boxes[boxIndex].innerText = value;
+    boxes[boxIndex].disabled = true;
     checkWinner();
 });
-    
+
+socket.on('reset', () => {
+    resetGame();  // Reset the game when the server sends a reset event
 });
+
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        if (box.innerText === "" && currentPlayerNumber !== undefined) {
+            const value = currentPlayerNumber === 0 ? "0" : "X";
+            box.innerText = value;
+            box.disabled = true;
+            // Emit move event with player information
+            socket.emit('move', { boxIndex: index, value });
+        }
+    });
+});
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
 const disableBoxes = () => {
-    for(let box of boxes) {
+    for (let box of boxes) {
         box.disabled = true;
     }
-}
+};
+
 const enableBoxes = () => {
-    for(let box of boxes) {
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
-}
+};
 
 const showWinner = (winner) => {
     msg.innerText = `Winner is ${winner}`;
     msgContainer.classList.remove("hide");
     disableBoxes();
-}
+};
 
 function checkWinner() {
-    for(let pattern of winPatterns) {
+    for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val != "" && pos2Val !="" && pos3Val != ""){
-            if(pos1Val == pos2Val && pos2Val == pos3Val){
-               
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 showWinner(pos1Val);
             }
         }
-    };
+    }
 };
-
-newGameBtn.addEventListener("click",resetGame);
-resetBtn.addEventListener("click",resetGame);
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.resolve('')))
+
+let players = []; // Array to store player information
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Assign player number (0 or 1) to the connected player
+    const playerNumber = players.length;
+    players.push({ id: socket.id, number: playerNumber });
+
+    // Notify the player about their assigned number
+    socket.emit('player-number', playerNumber);
+
+    // Broadcast the updated player list to all players
+    io.emit('update-players', players);
+
+    // Handle player move
+    socket.on('move', (data) => {
+        io.emit('update', data);
+    });
+
+    // Handle disconnect
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        // Remove disconnected player from the players array
+        players = players.filter(player => player.id !== socket.id);
+        // Broadcast the updated player list to all players
+        io.emit('update-players', players);
+    });
+});
+
+app.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(3000, () => {
+    console.log("listening on port 3000");
+});
